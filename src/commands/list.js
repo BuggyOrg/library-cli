4f2d6c4daf1834691c0478fe1a1252b9b367e5fb
Maximilian Klein <mklein@welfenlab.de>
2016-11-18 18:50:00 +0100

    (feature) Query list of components.

--- src/commands/list.js
+++ src/commands/list.js
@@ -1,9 +1,13 @@
 
-// import client from '@buggyorg/library-client'
+import {connect} from '@buggyorg/library-client'
 
 export const command = 'list'
 export const desc = 'List all components'
 export const handler = (argv) => {
   console.log('Listing all components')
   console.log(argv.server)
+  Promise.resolve(connect(argv.server))
+  .then((con) => con.components())
+  .then((comps) => console.log('Components: ', comps))
+  .catch((err) => console.error(err))
 }
