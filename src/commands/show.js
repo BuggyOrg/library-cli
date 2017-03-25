0b8662b82c5bb52154b6ad40dafc5bb993f03efe
Maximilian Klein <dasmaxl@gmail.com>
2017-03-25 21:41:39 +0100

    (fix) Show works with arbitrary prefixed commands now.

--- src/commands/show.js
+++ src/commands/show.js
@@ -1,6 +1,7 @@
 
 import {connect} from '@buggyorg/library-client'
 import {error, searchNode, log} from '../utils'
+import dropWhile from 'lodash/fp/dropWhile'
 
 export const command = 'show'
 export const desc = 'Shows the definition of a component.'
@@ -9,13 +10,14 @@ export const builder = (yargs) => {
     if (current === 'search') return []
     return searchNode(argv.library, current)
   })
-  .option('p', 'prettyprint')
+  .option('prettyprint', {alias: 'p'})
   .demand(1)
 }
 export const handler = (argv) => {
-  log(argv, 'Showing component:', argv._[1])
+  const args = dropWhile((a) => a !== 'show', argv._)
+  log(argv, 'Showing component:', args[1])
   return Promise.resolve(connect(argv.library))
-  .then((client) => client.component(argv._[1]))
+  .then((client) => client.component(args[1]))
   .then((res) => {
     if (argv.prettyprint) {
       console.log(JSON.stringify(res, null, 2))
